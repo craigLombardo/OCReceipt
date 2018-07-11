3c389b1a2fd3c3db7e844f7cded0169402640b3b
lombardc <lombardc@lafayette.edu>
2018-07-11 00:12:27 -0400

    Fixed issue with line items taking parent onClick call

--- app/src/main/java/com/craiglombardo/ocreceipt/OverviewPage.java
+++ app/src/main/java/com/craiglombardo/ocreceipt/OverviewPage.java
@@ -17,6 +17,8 @@ public class OverviewPage extends AppCompatActivity {
     private LinearLayout parentLinearLayout;
     private int screenHalf;
 
+    private Person expandedPerson = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,17 +67,21 @@ public class OverviewPage extends AppCompatActivity {
 
         final View firstView = inflater.inflate(R.layout.overview_line_header, null);
         final View secondView = inflater.inflate(R.layout.overview_line_header, null);
+        final View thirdView = inflater.inflate(R.layout.overview_line_header, null);
 
         Person p1 = new Person(firstView, "Craig");
         Person p2 = new Person(secondView, "Christer");
+        Person p3 = new Person(thirdView, "Some Random Name");
 
         for (int i = 0; i < 20; i++) {
             p1.addItem("Item: " + i, i);
             if (i % 2 == 0) p2.addItem("Item: " + i, i);
+            if (i % 3 == 0) p3.addItem("Item: " + i, i);
         }
 
         parentLinearLayout.addView(firstView, parentLinearLayout.getChildCount());
         parentLinearLayout.addView(secondView, parentLinearLayout.getChildCount());
+        parentLinearLayout.addView(thirdView, parentLinearLayout.getChildCount());
 
 //        p1.inflateItems();
 //        p2.inflateItems();
@@ -108,17 +114,15 @@ public class OverviewPage extends AppCompatActivity {
         private ArrayList<String> items;
         private ArrayList<Double> prices;
 
-        private View parent;
+        private LinearLayout listParent;
         private LinearLayout row;
 
         private Double totalCost;
 
-        private boolean expanded = false;
-
         private Person(View parentView, String name) {
 
-            parent = parentView;
-            row = parent.findViewById(R.id.line_header);
+            listParent = parentView.findViewById(R.id.line_item_list);
+            row = parentView.findViewById(R.id.line_header);
 
             TextView fullName = row.findViewById(R.id.line_name);
             fullName.setText(name);
@@ -128,17 +132,25 @@ public class OverviewPage extends AppCompatActivity {
             items = new ArrayList<>();
             prices = new ArrayList<>();
 
-            row.setOnClickListener(new View.OnClickListener() {
+            parentView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     toggle();
                 }
             });
+
+            listParent.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+//                    toggle();
+                }
+            });
         }
 
         private void toggle() {
-            if (expanded) collapseItems();
-            else inflateItems();
+            if (expandedPerson == null) inflateItems(false);
+            else if (expandedPerson == this) collapseItems();
+            else inflateItems(true);
         }
 
         private void addItem(String item, double cost) {
@@ -152,7 +164,7 @@ public class OverviewPage extends AppCompatActivity {
             return totalCost;
         }
 
-        private void inflateItems() {
+        private void inflateItems(boolean collapseOther) {
             LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
             for (int i = 0; i < items.size(); i++) {
@@ -164,20 +176,22 @@ public class OverviewPage extends AppCompatActivity {
                 final TextView itemTotal = itemView.findViewById(R.id.line_item_total);
                 itemTotal.setText("" + prices.get(i));
 
-                row.addView(itemView);
+                listParent.addView(itemView);
             }
 
             final View totalView = inflater.inflate(R.layout.overview_line_totals, null);
             final TextView sub = totalView.findViewById(R.id.overview_subtotal);
             sub.setText("" + totalCost);
-            row.addView(totalView);
+            listParent.addView(totalView);
 
-            expanded = true;
+            if (collapseOther) expandedPerson.collapseItems();
+
+            expandedPerson = this;
         }
 
         private void collapseItems() {
-            for (int i = row.getChildCount() - 1; i >= 2; i--) row.removeViewAt(i);
-            expanded = false;
+            for (int i = listParent.getChildCount() - 1; i >= 0; i--) listParent.removeViewAt(i);
+            expandedPerson = null;
         }
     }
 
